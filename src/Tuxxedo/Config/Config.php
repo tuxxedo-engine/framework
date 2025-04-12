a767d2c58b24a54d50bdec694e3f587f3959601c
Kalle Sommer Nielsen <kalle@php.net>
2025-04-12 19:28:01 +0200

    Bump copyright

--- src/Tuxxedo/Config/Config.php
+++ src/Tuxxedo/Config/Config.php
@@ -6,7 +6,7 @@
  * This file is part of the Tuxxedo Engine framework and is licensed under
  * the MIT license.
  *
- * Copyright (C) 2024 Kalle Sommer Nielsen <kalle@php.net>
+ * Copyright (C) 2025 Kalle Sommer Nielsen <kalle@php.net>
  */
 
 declare(strict_types=1);
