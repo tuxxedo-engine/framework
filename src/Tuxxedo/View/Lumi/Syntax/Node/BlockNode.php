28cbfdeba224886194931eb40851121b8de5b728
Kalle Sommer Nielsen <kalle@php.net>
2025-09-10 18:53:51 +0200

    Layout and Block nodes

---
+++ src/Tuxxedo/View/Lumi/Syntax/Node/BlockNode.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Tuxxedo Engine
+ *
+ * This file is part of the Tuxxedo Engine framework and is licensed under
+ * the MIT license.
+ *
+ * Copyright (C) 2025 Kalle Sommer Nielsen <kalle@php.net>
+ */
+
+declare(strict_types=1);
+
+namespace Tuxxedo\View\Lumi\Syntax\Node;
+
+readonly class BlockNode implements NodeInterface
+{
+    public string $kind;
+
+    /**
+     * @param NodeInterface[] $body
+     */
+    public function __construct(
+        public string $name,
+        public array $body,
+    ) {
+        $this->kind = BuiltinNodeKinds::ROOT->name;
+    }
+}
