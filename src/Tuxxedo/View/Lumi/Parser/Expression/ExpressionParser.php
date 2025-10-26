c0c8d4b525ecb892da170d64fc7b9159b1e122f1
Kalle Sommer Nielsen <kalle@php.net>
2025-10-26 22:07:37 +0100

    Update TODO

--- src/Tuxxedo/View/Lumi/Parser/Expression/ExpressionParser.php
+++ src/Tuxxedo/View/Lumi/Parser/Expression/ExpressionParser.php
@@ -502,7 +502,6 @@ class ExpressionParser implements ExpressionParserInterface
     private function parseArrayItems(
         TokenStreamInterface $stream,
     ): array {
-        // @todo Consider whether LHS=Identifier should be a shorthand for string in write context?
         $token = $stream->current();
 
         if (
