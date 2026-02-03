90d78aff8d01250f5af5a2bef4848a1a2f064b14
Kalle Sommer Nielsen <kalle@php.net>
2026-02-03 01:10:11 +0100

    endforeach is always standalone

--- src/Tuxxedo/View/Lumi/Lexer/Handler/Block/EndForEachBlockHandler.php
+++ src/Tuxxedo/View/Lumi/Lexer/Handler/Block/EndForEachBlockHandler.php
@@ -17,7 +17,7 @@ use Tuxxedo\View\Lumi\Lexer\Expression\ExpressionLexerInterface;
 use Tuxxedo\View\Lumi\Lexer\LexerStateInterface;
 use Tuxxedo\View\Lumi\Syntax\Token\EndForEachToken;
 
-class EndForEachBlockHandler implements BlockHandlerInterface
+class EndForEachBlockHandler implements BlockHandlerInterface, AlwaysStandaloneInterface
 {
     public private(set) string $directive = 'endforeach';
 
