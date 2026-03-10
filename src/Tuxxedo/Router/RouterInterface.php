140ca7751349db64b47d17c57058e479136b2671
Kalle Sommer Nielsen <kalle@php.net>
2026-03-10 01:13:17 +0100

    TODO

--- src/Tuxxedo/Router/RouterInterface.php
+++ src/Tuxxedo/Router/RouterInterface.php
@@ -18,6 +18,8 @@ use Tuxxedo\Http\Method;
 use Tuxxedo\Http\Request\RequestInterface;
 
 // @todo Consider an option to automatically add the final slash as an alias
+// @todo Consider an option to route for specific subdomains
+// @todo Consider an option for global prefixes like languages, api versions and such
 interface RouterInterface
 {
     /**
