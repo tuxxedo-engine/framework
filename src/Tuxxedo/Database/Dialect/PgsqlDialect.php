33cf91cfcfc9c855653ddf4efaf75dfac98ac41d
Kalle Sommer Nielsen <kalle@php.net>
2026-04-03 06:01:33 +0200

    SQL dialect interfaces

---
+++ src/Tuxxedo/Database/Dialect/PgsqlDialect.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Tuxxedo Engine
+ *
+ * This file is part of the Tuxxedo Engine framework and is licensed under
+ * the MIT license.
+ *
+ * Copyright (C) 2025 Kalle Sommer Nielsen <kalle@php.net>
+ */
+
+declare(strict_types=1);
+
+namespace Tuxxedo\Database\Dialect;
+
+class PgsqlDialect implements DialectInterface
+{
+    public private(set) array $quotations = [
+        '\'',
+        '"',
+    ];
+
+    public function placeholder(int $position): string
+    {
+        return '$' . $position;
+    }
+}
