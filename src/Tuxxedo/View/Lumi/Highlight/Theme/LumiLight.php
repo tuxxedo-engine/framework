7d2144aaebc7746479d504e0a687c6da88385098
Kalle Sommer Nielsen <kalle@php.net>
2026-04-08 18:52:13 +0200

    Bump copyright year

--- src/Tuxxedo/View/Lumi/Highlight/Theme/LumiLight.php
+++ src/Tuxxedo/View/Lumi/Highlight/Theme/LumiLight.php
@@ -6,7 +6,7 @@
  * This file is part of the Tuxxedo Engine framework and is licensed under
  * the MIT license.
  *
- * Copyright (C) 2025 Kalle Sommer Nielsen <kalle@php.net>
+ * Copyright (C) 2026 Kalle Sommer Nielsen <kalle@php.net>
  */
 
 declare(strict_types=1);
