dd72e15e1392a88a472950c1194a8da76ed346db
Kalle Sommer Nielsen <kalle@php.net>
2026-04-17 15:23:17 +0300

    Prep work for Integration tests

--- .dev/phpunit-coverage.bat
+++ .dev/phpunit-coverage.bat
@@ -2,5 +2,5 @@
 SET XDEBUG_MODE=coverage
 CD ../
 RD /s /q test-coverage
-CALL php85 "vendor/bin/phpunit" --coverage-html "test-coverage" --coverage-clover "test-coverage/clover.xml"
+CALL php85 "vendor/bin/phpunit" --testsuite Unit --coverage-html "test-coverage" --coverage-clover "test-coverage/clover.xml"
 CD .dev/
