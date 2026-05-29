2f12518617459a15ac868e06127b9745aa99cabe
Kalle Sommer Nielsen <kalle@php.net>
2026-05-30 01:19:55 +0300

    TODO

--- src/Tuxxedo/Http/Request/Middleware/MiddlewareInterface.php
+++ src/Tuxxedo/Http/Request/Middleware/MiddlewareInterface.php
@@ -16,6 +16,13 @@ namespace Tuxxedo\Http\Request\Middleware;
 use Tuxxedo\Http\Request\RequestInterface;
 use Tuxxedo\Http\Response\ResponseInterface;
 
+// @todo Implement TrustProxy for X-Forwarded-* parsing (likely needs Request::withServer())
+// @todo Implement Cors for cross-origin requests with preflight handling
+// @todo Implement RequestId for correlation IDs via X-Request-Id
+// @todo Implement MaintenanceMode for 503 with Retry-After
+// @todo Implement MethodOverride for _method/X-HTTP-Method-Override
+// @todo Implement MaxBodySize for 413 enforcement on Content-Length
+// @todo Implement AccessLog for request/response logging via LoggerInterface
 interface MiddlewareInterface
 {
     public function handle(
