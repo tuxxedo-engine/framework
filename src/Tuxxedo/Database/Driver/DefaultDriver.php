9ca8c684d231c7daf18ae2bbb44d999f76a3d19c
Kalle Sommer Nielsen <kalle@php.net>
2026-06-28 14:35:35 +0300

    Remove unused method

--- src/Tuxxedo/Database/Driver/DefaultDriver.php
+++ src/Tuxxedo/Database/Driver/DefaultDriver.php
@@ -13,14 +13,6 @@ declare(strict_types=1);
 
 namespace Tuxxedo\Database\Driver;
 
-use Tuxxedo\Database\Driver\Mysql\MysqlConnection;
-use Tuxxedo\Database\Driver\Pdo\Generic\PdoGenericConnection;
-use Tuxxedo\Database\Driver\Pdo\Mysql\PdoMysqlConnection;
-use Tuxxedo\Database\Driver\Pdo\Pgsql\PdoPgsqlConnection;
-use Tuxxedo\Database\Driver\Pdo\Sqlite\PdoSqliteConnection;
-use Tuxxedo\Database\Driver\Pgsql\PgsqlConnection;
-use Tuxxedo\Database\Driver\Sqlite\SqliteConnection;
-
 enum DefaultDriver
 {
     case MYSQL;
@@ -30,20 +22,4 @@ enum DefaultDriver
     case PDO_SQLITE;
     case PGSQL;
     case SQLITE;
-
-    /**
-     * @return class-string<ConnectionInterface>
-     */
-    public function getDriverClass(): string
-    {
-        return match ($this) {
-            self::MYSQL => MysqlConnection::class,
-            self::PDO => PdoGenericConnection::class,
-            self::PDO_MYSQL => PdoMysqlConnection::class,
-            self::PDO_PGSQL => PdoPgsqlConnection::class,
-            self::PDO_SQLITE => PdoSqliteConnection::class,
-            self::PGSQL => PgsqlConnection::class,
-            self::SQLITE => SqliteConnection::class,
-        };
-    }
 }
