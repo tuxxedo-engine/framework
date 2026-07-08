a3e3c040bf53b7f5d2da3f0d23b3ec084a995bc5
Kalle Sommer Nielsen <kalle@php.net>
2026-07-08 20:46:50 +0300

    Introduce the Temporal namespace which includes a basic clock for now

---
+++ src/Tuxxedo/Temporal/ClockInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Tuxxedo Engine
+ *
+ * This file is part of the Tuxxedo Engine framework and is licensed under
+ * the MIT license.
+ *
+ * Copyright (C) 2026 Kalle Sommer Nielsen <kalle@php.net>
+ */
+
+declare(strict_types=1);
+
+namespace Tuxxedo\Temporal;
+
+use Tuxxedo\Container\DefaultImplementation;
+use Tuxxedo\Container\Lifecycle;
+
+#[DefaultImplementation(class: SystemClock::class, lifecycle: Lifecycle::SINGLETON)]
+interface ClockInterface
+{
+    public function now(): \DateTimeImmutable;
+}
